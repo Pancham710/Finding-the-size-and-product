57d9741bfac332a77f17c9f1fd3995dfaa5ea33c
Pancham710 <70566369+Pancham710@users.noreply.github.com>
2021-01-16 09:17:00 +0530

    Create pancham.cpp

---
+++ pancham.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+using namespace std;   
+int main()  
+{  
+    char characterType ;
+    int integerType;   
+    float floatType;  
+    double doubleType;
+    cout << "Size of char is: " << sizeof(characterType)<<endl;   
+    cout << "Size of int is: " << sizeof(integerType) <<endl;   
+    cout << "Size of float is: " << sizeof(floatType) <<endl;   
+    cout << "Size of double is: " <<  sizeof(doubleType) <<endl; 
+    return 0;      
+}  
